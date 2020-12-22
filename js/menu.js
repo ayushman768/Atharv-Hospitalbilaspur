16a7fb88f6fb636648cc91a8b170dcef7c69b76f
Ayushmann <63894266+ayushman768@users.noreply.github.com>
2020-12-22 10:35:44 +0530

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,56 @@
+function My_menu(){
+	    var body,curv;
+		var menu = document.querySelector('.navigation');
+		
+			if(menu.style.display==="none"){
+				menu.style.display="block";
+			}
+			else{
+				menu.style.display="none";
+				
+			}
+	
+}
+
+
+var y = document.querySelector('header');
+var u = document.querySelector('.mywidget');
+
+var sticky = y.offsetTop;
+
+var header = function myheader() {
+
+    if (window.pageYOffset > sticky) {
+        y.classList.add('sticky');
+		u.classList.add('u');
+
+
+    } else {
+        y.classList.remove('sticky');
+		u.style.display="block";
+		u.classList.remove('u');
+
+    }
+
+};
+
+var y = document.querySelector('nav');
+var u = document.querySelector('.mywidget');
+
+var sticky = y.offsetTop;
+
+var nav = function mynav() {
+
+    if (window.pageYOffset > sticky) {
+        y.classList.add('sticky');
+		u.classList.add('u');
+
+
+    } else {
+        y.classList.remove('sticky');
+		u.style.display="block";
+		u.classList.remove('u');
+
+    }
+
+};
